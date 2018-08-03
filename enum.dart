a40c5dcab3391397d20fb2bd3ad23cdb4d73ba1e
Berk Batuhan Şakar <berkbatuhans@gmail.com>
2018-08-03 13:58:38 +0300

    data init

---
+++ enum.dart
@@ -0,0 +1,21 @@
+enum CurrencyType {
+  Euro,
+  TL,
+}
+
+enum MovementType {
+  Harcama,
+  GidenTransfer,
+  GelenTransfer,
+  IptalIade,
+  KrediKullandirim,
+  MasrafUcret,
+  NakitAvans,
+  Odeme,
+  ParaCekme,
+  ParaYatirma,
+  VergiKesintisi,
+  AltinAlisSatis,
+  Diger,
+  DovizAlisSatis,
+}
